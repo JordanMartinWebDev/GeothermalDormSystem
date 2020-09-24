89a16faf780f5eb8324a42ffda3bc31780d3d452
Logan Freeman <Logan.fr33man@gmail.com>
2020-09-24 12:18:17 -0500

    init three.js

---
+++ GeoDorms/three.js/examples/jsm/renderers/webgpu/WebGPUBinding.js
@@ -0,0 +1,21 @@
+class WebGPUBinding {
+
+	constructor( name = '' ) {
+
+		this.name = name;
+		this.visibility = null;
+
+		this.type = null; // read-only
+
+
+	}
+
+	setVisibility( visibility ) {
+
+		this.visibility = visibility;
+
+	}
+
+}
+
+export default WebGPUBinding;
